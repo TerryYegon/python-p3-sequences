ed6197861e618ae93f3cf728a57714aa7ed6ba42
Terry Yegon <yegonterry618@gmail.com>
2025-08-11 16:17:19 +0300

    sequences code

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+
+    if n <= 0:
+        print([])
+        return
+    elif n == 1:
+        print([0])
+        return
+
+    
+    fib_list = [0, 1]
+
+    
+    while len(fib_list) < n:
+        next_num = fib_list[-1] + fib_list[-2]  
+        fib_list.append(next_num)
+
+    print(fib_list)
